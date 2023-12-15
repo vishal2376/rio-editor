49a5a340bb636f347ea37be3adcef25916d3e1a5
Vishal Singh <vishalsingh2376@gmail.com>
2023-12-15 12:44:47 +0530

    chore: add simple multiline text container

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,28 @@
-use iced::{widget::text, Sandbox, Settings};
+use iced::{
+    widget::{container, text_editor},
+    Sandbox, Settings,
+};
 
 fn main() -> iced::Result {
     Editor::run(Settings::default())
 }
 
-struct Editor;
+struct Editor {
+    content: text_editor::Content,
+}
 
-#[derive(Debug)]
-enum Message {}
+#[derive(Debug, Clone)]
+enum Message {
+    Edit(text_editor::Action),
+}
 
 impl Sandbox for Editor {
     type Message = Message;
 
     fn new() -> Self {
-        Self
+        Editor {
+            content: text_editor::Content::new(),
+        }
     }
 
     fn title(&self) -> String {
@@ -21,10 +30,16 @@ impl Sandbox for Editor {
     }
 
     fn update(&mut self, message: Self::Message) {
-        match message {}
+        match message {
+            Message::Edit(action) => {
+                self.content.edit(action);
+            }
+        }
     }
 
     fn view(&self) -> iced::Element<'_, Self::Message> {
-        text("Hello world").into()
+        let input = text_editor(&self.content).on_edit(Message::Edit);
+
+        container(input).padding(20).into()
     }
 }
