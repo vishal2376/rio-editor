ce5a0498d92f33efce69125c3707429f19874036
Vishal Singh <vishalsingh2376@gmail.com>
2023-12-15 12:29:09 +0530

    gui hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{widget::text, Sandbox, Settings};
+
+fn main() -> iced::Result {
+    Editor::run(Settings::default())
+}
+
+struct Editor;
+
+#[derive(Debug)]
+enum Message {}
+
+impl Sandbox for Editor {
+    type Message = Message;
+
+    fn new() -> Self {
+        Self
+    }
+
+    fn title(&self) -> String {
+        String::from("Rio Editor")
+    }
+
+    fn update(&mut self, message: Self::Message) {
+        match message {}
+    }
+
+    fn view(&self) -> iced::Element<'_, Self::Message> {
+        text("Hello world").into()
+    }
 }
